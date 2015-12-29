c848d9f081e5681fa76cea5b11d4e6a48403bf81
Romain Calascibetta <romain.calascibetta@gmail.com>
2015-12-28 17:20:44 -0800

    Add encode function for quoted-printable

--- lib/quotedPrintable.ml
+++ lib/quotedPrintable.ml
@@ -17,6 +17,74 @@ struct
       Buffer.add_char buf chr
   end
 
+  module T =
+  struct
+    let _to = "0123456789ABCDEF"
+
+    type t =
+      {
+        word             : Buffer.t;
+        mutable position : int;
+      }
+
+    let make () =
+      { word = Buffer.create 16
+      ; position = 0 }
+
+    let add_break buf ({ position; _ } as t) =
+      if position > 0
+      then begin
+        Buffer.add_string buf "=\n";
+        t.position <- 0;
+      end
+
+    let commit_word buf ({ position; word; } as t) =
+      if position + Buffer.length word >= 76
+      then add_break buf t;
+
+      Buffer.add_buffer buf word;
+      t.position <- t.position + Buffer.length word;
+      Buffer.clear word
+
+    (* XXX: or TODO, does not handle a word with size > 76 *)
+    let wrap_bigword buf ({ word; _ } as t) length =
+      if Buffer.length word >= 76 then add_break buf t;
+
+      Buffer.add_buffer buf word;
+      t.position <- t.position + Buffer.length word;
+      Buffer.clear word
+
+    let add_char buf ({ word; _ } as t) chr =
+      wrap_bigword buf t 1;
+      Buffer.add_char word chr
+
+    let add_quoted_char buf ({ word; _ } as t) chr =
+      wrap_bigword buf t 3;
+
+      let code = Char.code chr in
+      let h    = (code lsr 4) land (16 - 1) in
+      let l    =  code        land (16 - 1) in
+
+      Buffer.add_char word '=';
+      Buffer.add_char word _to.[h];
+      Buffer.add_char word _to.[l]
+
+    let add_wsp buf t chr =
+      add_char buf t chr;
+      commit_word buf t
+
+    let add_newline buf t chr =
+      match chr with
+      | Some chr -> add_quoted_char buf t chr
+      | None -> ();
+      commit_word buf t;
+      Buffer.add_char buf '\n';
+      t.position <- 0
+
+    let flush buf t =
+      commit_word buf t
+  end
+
   let hex        = [%sedlex.regexp? '0' .. '9' | 'a' .. 'f' | 'A' .. 'F']
   let strict_hex = [%sedlex.regexp? '0' .. '9' | 'A' .. 'F']
 
@@ -95,4 +163,28 @@ struct
     | _ ->
       F.add_char buf
         (S.lexeme lexbuf |> fun s -> (* assert (String.length s = 1)); *) String.get s 0)
+
+  let rec encode buf ?(acc = T.make ()) lexbuf = match%sedlex lexbuf with
+    | Opt wsp, '\n' ->
+      if S.sub_lexeme lexbuf 0 1 = "\n"
+      then T.add_newline buf acc None
+      else T.add_newline buf acc (Some (S.sub_lexeme lexbuf 0 1 |> fun s -> String.get s 0));
+
+      encode buf ~acc lexbuf
+
+    | wsp ->
+      T.add_wsp buf acc (S.lexeme lexbuf |> fun s -> (* assert (String.length s = 1)); *) String.get s 0);
+
+      encode buf ~acc lexbuf
+
+    | safe_char ->
+      T.add_char buf acc (S.lexeme lexbuf |> fun s -> (* assert (String.length s = 1)); *) String.get s 0);
+
+      encode buf ~acc lexbuf
+
+    | eof -> ()
+    | _ ->
+      T.add_quoted_char buf acc (S.lexeme lexbuf |> fun s -> (* assert (String.length s = 1)); *) String.get s 0);
+
+      encode buf ~acc lexbuf
 end
