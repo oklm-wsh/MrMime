b683c8230e71be39933ca5f47091c6d5307bcef8
Romain Calascibetta <romain.calascibetta@gmail.com>
2016-04-27 08:27:19 -0700

    Remove not usefull interface of RFC

--- lib/rfc6854.mli
+++
@@ -1,8 +0,0 @@
-open Rfc5322
-
-val p_from            : (address list -> Lexer.t -> ([> Lexer.err | 'ret Lexer.read ] as 'ret)) -> Lexer.t -> 'ret
-val p_sender          : (address      -> Lexer.t -> ([> Lexer.err | 'ret Lexer.read ] as 'ret)) -> Lexer.t -> 'ret
-val p_reply_to        : (address list -> Lexer.t -> ([> Lexer.err | 'ret Lexer.read ] as 'ret)) -> Lexer.t -> 'ret
-
-val p_resent_from     : (address list -> Lexer.t -> ([> Lexer.err | 'ret Lexer.read ] as 'ret)) -> Lexer.t -> 'ret
-val p_resent_sender   : (address      -> Lexer.t -> ([> Lexer.err | 'ret Lexer.read ] as 'ret)) -> Lexer.t -> 'ret
--- lib/rfc822.mli
+++
@@ -1,4 +0,0 @@
-val is_space : char -> bool
-val is_ctl   : char -> bool
-val is_digit : char -> bool
-val is_lwsp  : char -> bool
