ccc0904b9c6c715ba10e0c38c60c360dbf6ee0e2
Romain Calascibetta <romain.calascibetta@gmail.com>
2016-04-29 04:03:12 -0700

    Download IANA database at compile time

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -6,19 +6,27 @@ open Ocamlbuild_plugin
 let env_filename = Pathname.basename BaseEnvLight.default_filename
 let env          = BaseEnvLight.load ~filename:env_filename ~allow_empty:true ()
 let iana         = bool_of_string @@ BaseEnvLight.var_get "iana" env
+let update       = bool_of_string @@ BaseEnvLight.var_get "update" env
 
 let ppx_iana = "ppx/ppx_iana.byte"
-let cmd_iana x = ppx_iana
+let get_iana = "ppx/get_iana.byte"
+let cmd_iana = ppx_iana
+
+let () =
+  rule "iana"
+    ~prod:"iana.xml"
+    ~dep:get_iana
+    (fun env build -> Cmd (S [A get_iana]))
 
 let () = dispatch
   (function
    | After_hygiene ->
-     if iana then dep ["ppx_iana"] ["iana.xml"];
+     dep ["ppx_iana"] ["iana.xml"];
      dep ["ppx_iana"] [ppx_iana];
 
-     flag [ "ocaml"; "ocamldep"; "ppx_iana" ] (S [A "-ppx"; A (cmd_iana iana)]);
-     flag [ "ocaml"; "compile"; "ppx_iana" ]  (S [A "-ppx"; A (cmd_iana iana)]);
-     flag [ "ocaml"; "link"; "ppx_iana" ]     (S [A "-ppx"; A (cmd_iana iana)]);
+     flag [ "ocaml"; "ocamldep"; "ppx_iana" ] (S [A "-ppx"; A cmd_iana]);
+     flag [ "ocaml"; "compile"; "ppx_iana" ]  (S [A "-ppx"; A cmd_iana]);
+     flag [ "ocaml"; "link"; "ppx_iana" ]     (S [A "-ppx"; A cmd_iana]);
 
      dispatch_default After_hygiene
    | x -> dispatch_default x)
