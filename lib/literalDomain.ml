0405d169b146926361905c11b599ca99843a7004
Romain Calascibetta <romain.calascibetta@gmail.com>
2016-06-13 07:14:27 -0700

    Better interface for LiteralDomain module with new Encoder

--- lib/literalDomain.ml
+++ lib/literalDomain.ml
@@ -1,44 +1,120 @@
-open BasePrinter
-
 type t =
   [ `General of string * string
   | `IPv4 of Ipaddr.V4.t
   | `IPv6 of Ipaddr.V6.t ]
 
-let of_string s =
-  let rec loop = function
-    | `Error (exn, buf, off, len) ->
-      let tmp = Buffer.create 16 in
-      let fmt = Format.formatter_of_buffer tmp in
+module D =
+struct
+  open BaseDecoder
 
-      Format.fprintf fmt "%a (buf: %S)%!"
-        Error.pp exn (Bytes.sub buf off (len - off));
+  let of_decoder state =
+    let rec loop = function
+      | `Error (exn, buf, off, len) ->
+        let tmp = Buffer.create 16 in
+        let fmt = Format.formatter_of_buffer tmp in
 
-      raise (Invalid_argument ("Address.of_string: " ^ (Buffer.contents tmp)))
-    | `Read (buf, off, len, k) ->
-      raise (Invalid_argument "Address.of_string: unterminated string")
-    | `Ok data -> data
-  in
+        Format.fprintf fmt "%a (buf: %S)%!"
+          Error.pp exn (Bytes.sub buf off (len - off));
 
-  let rule = Rfc5321.p_address_literal
-    (fun data -> Rfc822.p_crlf (fun _ -> `Ok data)) in
-  loop @@ BaseDecoder.safe rule (Decoder.of_string (s ^ "\r\n\r\n"))
+        raise (Invalid_argument ("LiteralDomain.of_string: " ^ (Buffer.contents tmp)))
+      | `Read (buf, off, len, k) ->
+        raise (Invalid_argument "Address.of_string unterminated string")
+      | `Ok data -> data
+    in
 
-let p = Format.fprintf
+    let rule =
+      Rfc5321.p_address_literal
+      @ fun data -> Rfc822.p_crlf
+      @ Rfc822.p_crlf
+      @ fun _ -> `Ok data
+    in
 
-let pp fmt = function
-  | `General (tag, content) ->
-    p fmt "%a:%a"
-      (pp_string ~in_qs:false ~in_dm:true) tag
-      (pp_string ~in_qs:false ~in_dm:true) content
-  | `IPv4 ip -> Ipaddr.V4.pp_hum fmt ip
-  | `IPv6 ip -> p fmt "IPv6:%a" Ipaddr.V6.pp_hum ip
+    loop @@ safe rule state
+end
 
+module E =
+struct
+  module Internal =
+  struct
+    open BaseEncoder
+    open Wrap
+
+    let explode str =
+      let rec exp i l =
+        if i < 0 then l else exp (i - 1) (str.[i] :: l) in
+      exp (String.length str - 1) []
+
+    let w_safe_string str =
+      List.fold_right
+        (function
+         | '\x00' -> w_string "\\\000"
+         | '\x07' -> w_string "\\a"
+         | '\x08' -> w_string "\\b"
+         | '\x09' -> w_string "\\t"
+         | '\x0A' -> w_string "\\n"
+         | '\x0B' -> w_string "\\v"
+         | '\x0C' -> w_string "\\f"
+         | '\x0D' -> w_string "\\r"
+         | '\\'   -> w_string "\\\\"
+         | '"'    -> w_string "\\\""
+         | ' '    -> w_string " "
+         | ']'    -> w_string "\\]"
+         | chr    ->
+           if Rfc5322.is_vchar chr
+           then w_char chr
+           else w_string (sp "\\%c" chr))
+        (explode str)
+
+    let w_domain = function
+      | `General (tag, value) ->
+        w_hovbox 1
+        $ w_string "["
+        $ w_string tag
+        $ w_string ":"
+        $ w_safe_string value
+        $ w_string "]"
+        $ w_close_box
+      | `IPv4 ipv4 ->
+        w_hovbox 1
+        $ w_string "["
+        $ w_string (Ipaddr.V4.to_string ipv4)
+        $ w_string "]" $ w_close_box
+      | `IPv6 ipv6 ->
+        w_hovbox 1
+        $ w_string "["
+        $ w_string "IPv6"
+        $ w_string ":"
+        $ w_string (Ipaddr.V6.to_string ipv6)
+        $ w_string "]"
+        $ w_close_box
+  end
+
+  let w = Internal.w_domain
+
+  let to_buffer t state =
+    let buf = Buffer.create 16 in
+
+    let rec loop = function
+      | `Partial (s, i, l, k) ->
+        Buffer.add_subbytes buf s i l;
+        loop @@ (k l)
+      | `Ok -> buf
+    in
+
+    let rule =
+      let open BaseEncoder in
+      let ok = flush (fun _ -> `Ok) in
+      Wrap.lift Wrap.(Internal.w_domain t (unlift ok))
+    in
+
+    loop @@ rule state
+end
+
+let of_string s = D.of_decoder (Decoder.of_string (s ^ "\r\n\r\n"))
+let to_string t = Buffer.contents @@ E.to_buffer t (Encoder.make ())
+
+let pp fmt _ = Format.fprintf fmt "#literal-domain"
 let equal = (=)
 
 let size domain =
-  let buf = Buffer.create 16 in
-  let fmt = Format.formatter_of_buffer buf in
-
-  Format.fprintf fmt "%a%!" pp domain;
-  Buffer.length buf
+  String.length @@ to_string domain
--- lib/literalDomain.mli
+++ lib/literalDomain.mli
@@ -3,7 +3,21 @@ type t =
   | `IPv4 of Ipaddr.V4.t
   | `IPv6 of Ipaddr.V6.t ]
 
+module D :
+sig
+  val of_decoder : Decoder.t -> t
+end
+
+module E :
+sig
+  val w : (t, 'r Encoder.partial) Wrap.k1
+  val to_buffer : t -> Encoder.t -> Buffer.t
+end
+
+val to_string : t -> string
 val of_string : string -> t
+
 val pp        : Format.formatter -> t -> unit
 val equal     : t -> t -> bool
+
 val size      : t -> int
