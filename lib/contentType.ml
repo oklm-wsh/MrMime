98f2daf54bd76ea3a9fba94b9449c83e81c85c3b
Romain Calascibetta <romain.calascibetta@gmail.com>
2016-06-13 07:18:58 -0700

    Better interface for ContentType module with new Encoder

--- lib/contentType.ml
+++ lib/contentType.ml
@@ -1,5 +1,3 @@
-open BasePrinter
-
 type ty    = Rfc2045.ty
 type subty = Rfc2045.subty
 type value = Rfc2045.value
@@ -45,39 +43,88 @@ let default =
   { ty = `Text; subty = `Iana_token "plain"
   ; parameters = ["charset", `Token "us-ascii"] }
 
-let of_lexer (ty, subty, parameters) =
-  { ty; subty; parameters; }
+type field = [ `ContentType of t ]
 
-let p = Format.fprintf
+let field_of_lexer = function
+  | `ContentType (ty, subty, parameters ) -> `ContentType { ty; subty; parameters; }
 
-let pp_ty fmt = function
-  | `Application  -> Format.pp_print_string fmt "application"
-  | `Audio        -> Format.pp_print_string fmt "audio"
-  | `Ietf_token s -> p fmt "%s" s
-  | `Image        -> Format.pp_print_string fmt "image"
-  | `Message      -> Format.pp_print_string fmt "message"
-  | `Multipart    -> Format.pp_print_string fmt "multipart"
-  | `Text         -> Format.pp_print_string fmt "text"
-  | `Video        -> Format.pp_print_string fmt "video"
-  | `X_token s    -> p fmt "%s" s
+module D =
+struct
+  let of_lexer (ty, subty, parameters) p state =
+    p { ty; subty; parameters; } state
+  let of_lexer' (ty, subty, parameters) =
+    { ty; subty; parameters; }
+end
 
-let pp_subty fmt = function
-  | `Ietf_token s -> p fmt "%s" s
-  | `Iana_token s -> p fmt "%s" s
-  | `X_token s    -> p fmt "%s" s
+module E =
+struct
+  module Internal =
+  struct
+    open BaseEncoder
+    open Wrap
 
-let pp_value fmt = function
-  | `String s -> p fmt "\"%a\"" (pp_string ~in_qs:true ~in_dm:false) s
-  | `Token s  -> Format.pp_print_string fmt s
+    let w_ty = function
+      | `Application  -> w_string "application"
+      | `Audio        -> w_string "audio"
+      | `Ietf_token s -> w_string s
+      | `Image        -> w_string "image"
+      | `Message      -> w_string "message"
+      | `Multipart    -> w_string "multipart"
+      | `Text         -> w_string "text"
+      | `Video        -> w_string "video"
+      | `X_token s    -> w_string s
 
-let pp fmt { ty; subty; parameters; } =
-  let rec aux fmt = function
-    | [] -> ()
-    | (attribute, value) :: r ->
-      Format.fprintf fmt "; %s=%a" attribute pp_value value;
-      aux fmt r
-  in
-  Format.fprintf fmt "%a/%a%a"
-    pp_ty ty pp_subty subty aux parameters
+    let w_subty = function
+      | `Ietf_token s -> w_string s
+      | `Iana_token s -> w_string s
+      | `X_token s    -> w_string s
+
+    let w_value = function
+      | `String s -> Address.E.w_safe_string s
+      | `Token s  -> w_string s
+
+    let w_parameter (key, value) =
+      w_hovbox 1
+      $ w_string key
+      $ w_close_box
+      $ w_char '='
+      $ w_hovbox 1
+      $ w_value value
+      $ w_close_box
+
+    let w_content { ty; subty; parameters; } =
+      let w_lst w_sep w_data l =
+        let rec aux = function
+          | [] -> noop
+          | x :: r -> w_sep $ w_hovbox 1 $ w_data x $ w_close_box $ aux r
+        in aux l
+      in
+      w_hovbox 1
+      $ w_ty ty
+      $ w_close_box
+      $ w_char '/'
+      $ w_hovbox 1
+      $ w_subty subty
+      $ w_close_box
+      $ w_hovbox 1
+      $ w_lst (w_char ';' $ w_space) w_parameter parameters
+      $ w_close_box
+      $ w_close_box
+
+    let w_crlf k e = w "\r\n" k e
+
+    let w_field = function
+      | `ContentType t ->
+        w "Content-Type: "
+        $ Wrap.lift
+        $ Wrap.(fun k -> (w_hovbox (String.length "Content-Type: ")
+                          $ w_content t
+                          $ w_close_box) (unlift k))
+        $ w_crlf
+  end
+
+  let w = Internal.w_field
+end
 
 let equal = (=)
+let pp fmt _ = Format.fprintf fmt "#content-type"
--- lib/contentType.mli
+++ lib/contentType.mli
@@ -9,8 +9,21 @@ val parameters : t -> (string * Rfc2045.value) list
 
 val make : ?parameters:(string * value) list -> ty -> subty -> t
 val default : t
-val of_lexer : Rfc2045.content -> t
 
-val pp : Format.formatter -> t -> unit
+type field = [ `ContentType of t ]
+
+val field_of_lexer : [ `ContentType of Rfc2045.content ] -> field
+
+module D :
+sig
+  val of_lexer  : Rfc2045.content -> (t, 'r) Decoder.k1
+  val of_lexer' : Rfc2045.content -> t
+end
+
+module E :
+sig
+  val w : (field, 'r Encoder.partial) Encoder.k1
+end
 
 val equal : t -> t -> bool
+val pp    : Format.formatter -> t -> unit
