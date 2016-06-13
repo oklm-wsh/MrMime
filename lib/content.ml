118a1b646add145e570502f9e260e34f53a521a7
Romain Calascibetta <romain.calascibetta@gmail.com>
2016-06-13 07:21:54 -0700

    Better interface for Content module with new Encoder

--- lib/content.ml
+++ lib/content.ml
@@ -6,22 +6,6 @@ type t =
   ; description : string option
   ; content     : (string * Rfc5322.phrase) list }
 
-type field =
-  [ `ContentType of ContentType.t
-  | `ContentEncoding of ContentEncoding.t
-  | `ContentID of MsgID.t
-  | `ContentDescription of string
-  | `Content of string * Rfc5322.phrase
-  | `Unsafe of string * Rfc5322.phrase ]
-
-let field_of_lexer : Rfc2045.field -> field = function
-  | `Content p -> `Content p
-  | `ContentDescription s -> `ContentDescription s
-  | `ContentType t -> `ContentType (ContentType.of_lexer t)
-  | `ContentEncoding e -> `ContentEncoding (ContentEncoding.of_lexer e)
-  | `ContentID i -> `ContentID (MsgID.of_lexer i)
-  | `Unsafe (field, value) -> `Unsafe (field, value)
-
 let ty { ty; _ } = ty
 let encoding { encoding; _ } = encoding
 
@@ -33,72 +17,176 @@ let make
   ?(extension = []) () =
   { ty; encoding; version; id; description; content = extension; }
 
-let of_lexer fields p state =
-  [%debug Printf.printf "state: of_lexer (content)\n%!"];
+module Part =
+struct
+  type field =
+    [ ContentType.field
+    | ContentEncoding.field
+    | `ContentID          of MsgID.t
+    | `ContentDescription of string
+    | `Content            of string * Rfc5322.phrase
+    | `Unsafe             of string * Rfc5322.phrase ]
 
-  let ty          = ref None in
-  let encoding    = ref None in
-  let version     = ref None in
-  let id          = ref None in
-  let description = ref None in
-  let content     = ref [] in
+  let field_of_lexer = function
+    | `Content p                -> `Content p
+    | `ContentDescription s     -> `ContentDescription s
+    | (`ContentType _) as x     -> (ContentType.field_of_lexer x :> field)
+    | (`ContentEncoding _) as x -> (ContentEncoding.field_of_lexer x :> field)
+    | `ContentID i              -> `ContentID (MsgID.D.of_lexer i)
+    | `Unsafe (field, value)    -> `Unsafe (field, value)
 
-  let sanitize fields =
-    p ({ ty          = Option.value ~default:ContentType.default !ty
-       ; encoding    = Option.value ~default:ContentEncoding.default !encoding
-       ; version     = Option.value ~default:MimeVersion.default !version
-       ; id          = !id
-       ; description = !description
-       ; content     = !content }) fields state
-  in
+  let to_field t : field list =
+    let ( >>= ) o f = match o with Some x -> Some (f x) | None -> None in
+    let ( @:@ ) o r = match o with Some x -> x :: r | None -> r in
 
-  let rec loop garbage fields = match fields with
-    | [] -> sanitize (List.rev garbage)
-    | field :: rest ->
-      match field with
-      | `ContentType c ->
-        (match !ty with
-         | None   -> ty := Some (ContentType.of_lexer c);
-                     loop garbage rest
-         | Some _ -> loop (field :: garbage) rest)
-      | `ContentEncoding e ->
-        (match !encoding with
-         | None   -> encoding := Some (ContentEncoding.of_lexer e);
-                     loop garbage rest
-         | Some _ -> loop (field :: garbage) rest)
-      | `ContentID e ->
-        (match !id with
-         | None   -> id := Some (MsgID.of_lexer e);
-                     loop garbage rest
-         | Some _ -> loop (field :: garbage) rest)
-      | `ContentDescription s ->
-        (match !description with
-         | None   -> description := Some s;
-                     loop garbage rest
-         | Some _ -> loop (field :: garbage) rest)
-      | `Content (field, value) ->
-        content := (field, value) :: !content;
-        loop garbage rest
-      | #Rfc2045.mime_field as v ->
-        (match !version with
-         | None   -> version := Some (MimeVersion.of_lexer v);
-                     loop garbage rest
-         | Some _ -> loop (field :: garbage) rest)
-      | field -> loop (field :: garbage) rest
-  in
+    (t.id >>= fun m -> `ContentID m)
+    @:@ (t.description >>= fun s -> `ContentDescription s)
+    @:@ (`ContentType t.ty) :: (`ContentEncoding t.encoding) :: []
+    @ (List.map (fun (key, value) -> `Content (key, value)) t.content)
+    @ (List.map (fun (key, value) -> `Unsafe (key, value)) [])
 
-  loop [] fields
+  module D =
+  struct
+    let of_lexer' extend fields p state =
+      [%debug Printf.printf "state: of_lexer (content)\n%!"];
 
-let p = Format.fprintf
+      let ty          = ref None in
+      let encoding    = ref None in
+      let id          = ref None in
+      let description = ref None in
+      let content     = ref [] in
 
-let pp fmt { ty; encoding; version; id; } =
-  let pp_field_opt fmt field_name pp_field field_opt =
-    match field_opt with
-    | Some field -> p fmt "%s: %a\r\n" field_name pp_field field
-    | None       -> ()
-  in
+      let sanitize fields =
+        p ({ ty          = Option.value ~default:ContentType.default !ty
+           ; encoding    = Option.value ~default:ContentEncoding.default !encoding
+           ; version     = MimeVersion.default
+           ; id          = !id
+           ; description = !description
+           ; content     = !content }) fields state
+      in
 
-  p fmt "MIME-Version: %a\r\n" MimeVersion.pp version;
-  p fmt "Content-Type: %a\r\n" ContentType.pp ty;
-  p fmt "Content-Transfer-Encoding: %a\r\n" ContentEncoding.pp encoding;
-  pp_field_opt fmt "Content-ID" MsgID.pp id;
+      let rec loop garbage fields = match fields with
+        | [] -> sanitize (List.rev garbage)
+        | field :: rest ->
+          match field with
+          | `ContentType c ->
+            (match !ty with
+             | None   -> ty := Some (ContentType.D.of_lexer' c);
+                         loop garbage rest
+             | Some _ -> loop (field :: garbage) rest)
+          | `ContentEncoding e ->
+            (match !encoding with
+             | None   -> encoding := Some (ContentEncoding.D.of_lexer' e);
+                         loop garbage rest
+             | Some _ -> loop (field :: garbage) rest)
+          | `ContentID e ->
+            (match !id with
+             | None   -> id := Some (MsgID.D.of_lexer e);
+                         loop garbage rest
+             | Some _ -> loop (field :: garbage) rest)
+          | `ContentDescription s ->
+            (match !description with
+             | None   -> description := Some s;
+                         loop garbage rest
+             | Some _ -> loop (field :: garbage) rest)
+          | `Content (field, value) ->
+            content := (field, value) :: !content;
+            loop garbage rest
+          | field -> extend loop field garbage rest
+      in
+
+      loop [] fields
+
+    let of_lexer fields p state =
+      of_lexer' (fun self field garbage rest -> self (field :: garbage) rest) fields p state
+  end
+
+  module E =
+  struct
+    module Internal =
+    struct
+      open BaseEncoder
+      open Wrap
+
+      let w_crlf k e = w "\r\n" k e
+
+      let w_content = function
+        | #ContentType.field as x -> ContentType.E.w x
+        | #ContentEncoding.field as x -> ContentEncoding.E.w x
+        | `ContentID m ->
+          w "Content-ID: "
+          $ Wrap.lift
+          $ Wrap.(fun k -> (w_hovbox (String.length "Content-ID: ")
+                            $ MsgID.E.w m
+                            $ w_close_box) (unlift k))
+          $ w_crlf
+        | `ContentDescription s ->
+          w "Content-Description: "
+          $ Wrap.lift
+          $ Wrap.(fun k -> (w_hovbox (String.length "Content-Description: ")
+                            $ w_string s
+                            $ w_close_box) (unlift k))
+          $ w_crlf
+        | `Content (field, value) ->
+          w ("Content-" ^ field) $ w ":"
+          $ Wrap.lift
+          $ Wrap.(fun k -> (w_hovbox (String.length field + 10)
+                            $ Address.E.w_phrase value
+                            $ w_close_box) (unlift k))
+          $ w_crlf
+        | `Unsafe (key, value) ->
+          w key $ w ":"
+          $ Wrap.lift
+          $ Wrap.(fun k -> (w_hovbox (String.length key + 2)
+                            $ Address.E.w_phrase value
+                            $ w_close_box) (unlift k))
+          $ w_crlf
+    end
+
+    let w_field = Internal.w_content
+    let w fields = List.fold_right Internal.w_content fields
+  end
+end
+
+module Message =
+struct
+  type field = [ Part.field | MimeVersion.field ]
+
+  let field_of_lexer = function
+    | #Rfc2045.field as x -> (Part.field_of_lexer x :> field)
+    | #Rfc2045.mime_field as x -> (MimeVersion.field_of_lexer x :> field)
+
+  let to_field t =
+    (`MimeVersion t.version :> field) :: (Part.to_field t :> field list)
+
+  module D =
+  struct
+    let of_lexer fields p =
+      let version = ref None in
+
+      Part.D.of_lexer'
+        (fun self field garbage rest ->
+         match field with
+         | `MimeVersion x ->
+           (match !version with
+            | None -> version := Some (MimeVersion.D.of_lexer' x);
+                      self garbage rest
+            | Some _ -> self (field :: garbage) rest)
+         | field -> self (field :: garbage) rest)
+        fields
+        (fun content -> p { content with version = Option.value !version ~default:MimeVersion.default })
+  end
+
+  module E =
+  struct
+    module Internal =
+    struct
+      let w_content = function
+        | #Part.field as x -> Part.E.w_field x
+        | #MimeVersion.field as x -> MimeVersion.E.w x
+    end
+
+    let w_field = Internal.w_content
+    let w fields = List.fold_right Internal.w_content fields
+  end
+end
---
+++ lib/content.mli
@@ -0,0 +1,48 @@
+type t
+
+val ty : t -> ContentType.t
+val encoding : t -> ContentEncoding.t
+
+module Part :
+sig
+  type field =
+    [ `ContentType        of ContentType.t
+    | `ContentEncoding    of ContentEncoding.t
+    | `ContentID          of MsgID.t
+    | `ContentDescription of string
+    | `Content            of string * Rfc5322.phrase
+    | `Unsafe             of string * Rfc5322.phrase ]
+
+  val field_of_lexer : Rfc2045.field -> field
+  val to_field       : t -> field list
+
+  module D :
+  sig
+    val of_lexer : ([> Rfc2045.field ] as 'content) list -> (t, 'content list, 'r) Decoder.k2
+  end
+
+  module E :
+  sig
+    val w_field : (field, 'r Encoder.partial) Encoder.k1
+    val w : (field list, 'r Encoder.partial) Encoder.k1
+  end
+end
+
+module Message :
+sig
+  type field = [ Part.field | MimeVersion.field ]
+
+  val field_of_lexer : [ Rfc2045.field | Rfc2045.mime_field ] -> field
+  val to_field       : t -> field list
+
+  module D :
+  sig
+    val of_lexer : ([> Rfc2045.field | Rfc2045.mime_field ] as 'content) list -> (t, 'content list, 'r) Decoder.k2
+  end
+
+  module E :
+  sig
+    val w_field : (field, 'r Encoder.partial) Encoder.k1
+    val w : (field list, 'r Encoder.partial) Encoder.k1
+  end
+end
